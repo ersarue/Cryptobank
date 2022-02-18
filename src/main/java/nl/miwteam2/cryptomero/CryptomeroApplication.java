6bc08c859f02a5327c0a4ef91b6bd57199469c1c
Marcel Brachten <marcel.brachten@hva.nl>
2022-02-18 16:01:37 +0100

    testMarcel

--- src/main/java/nl/miwteam2/cryptomero/CryptomeroApplication.java
+++ src/main/java/nl/miwteam2/cryptomero/CryptomeroApplication.java
@@ -2,7 +2,7 @@ package nl.miwteam2.cryptomero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//command
+//command nikeuw Marcel
 @SpringBootApplication
 public class CryptomeroApplication {
 
