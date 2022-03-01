6d4a4d245c8a70fa695c4c7411da253664770994
Petra Coenen <petra.coenen@hva.nl>
2022-03-01 15:18:01 +0100

    Add initial Address domain, dao, service and controller class

---
+++ src/main/java/nl/miwteam2/cryptomero/repository/JdbcAddressDao.java
@@ -0,0 +1,61 @@
+package nl.miwteam2.cryptomero.repository;
+
+import nl.miwteam2.cryptomero.domain.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author Petra Coenen
+ * @version 1.0
+ */
+
+@Repository
+public class JdbcAddressDao implements GenericDao<Address> {
+
+    private final Logger logger = LoggerFactory.getLogger(JdbcAddressDao.class);
+
+    JdbcTemplate jdbcTemplate;
+
+    public JdbcAddressDao(JdbcTemplate jdbcTemplate) {
+        super();
+        this.jdbcTemplate = jdbcTemplate;
+        logger.info("New JdbcAddressDao");
+    }
+
+    public void storeOne(Address address) {
+        String sql = "INSERT INTO address(street_name, house_no, house_add, postal_code, city) " +
+                "VALUES (?,?,?,?,?); ";
+        jdbcTemplate.update(sql, address.getStreetName(), address.getHouseNo(),
+                address.getHouseAdd(), address.getPostalCode(), address.getCity() );
+    }
+
+    public Address findById(int id) {
+        String sql = "SELECT * FROM address WHERE id_address = ?;";
+        return jdbcTemplate.queryForObject(sql, new AddressRowMapper(), id);
+    }
+
+    public List<Address> getAll() {
+        String sql = "SELECT * FROM address;";
+        return jdbcTemplate.query(sql, new AddressRowMapper(), null);
+    }
+
+    private class AddressRowMapper implements RowMapper<Address> {
+        @Override
+        public Address mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+            return new Address(resultSet.getInt("id_address"), resultSet.getString("street_name"),
+                    resultSet.getInt("house_no"), resultSet.getString("house_add"),
+                    resultSet.getString("postal_code"), resultSet.getString("city"));
+        }
+    }
+
+    public void updateAddress(Address address) {}
+
+    public void deleteAddress(int id) {}
+}
