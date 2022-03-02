43a583d2f860e3b0ee1ecdfbef951e38f9dffa7e
Marcel Brachten <marcel.brachten@hva.nl>
2022-03-02 15:16:56 +0100

    bankAccountController en service

--- src/main/java/nl/miwteam2/cryptomero/service/BankAccountService.java
+++ src/main/java/nl/miwteam2/cryptomero/service/BankAccountService.java
@@ -1,4 +1,22 @@
 package nl.miwteam2.cryptomero.service;
 
+import nl.miwteam2.cryptomero.domain.BankAccount;
+import nl.miwteam2.cryptomero.repository.JdbcBankAccountDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BankAccountService {
+    private JdbcBankAccountDao jdbcBankAccountDao;
+
+    @Autowired
+    public BankAccountService(JdbcBankAccountDao jdbcBankAccountDao){
+        this.jdbcBankAccountDao=jdbcBankAccountDao;
+    }
+    public List<BankAccount> getAllAccounts(){
+        return jdbcBankAccountDao.getAll();
+    }
+
 }
