cf781c508a13370f4f19e292219ce64945c0c795
ee <47424782+ee-git@users.noreply.github.com>
2022-03-02 12:02:06 +0100

    intializing domain, repository, controller and service

--- src/main/java/nl/miwteam2/cryptomero/controller/UserAccountController.java
+++ src/main/java/nl/miwteam2/cryptomero/controller/UserAccountController.java
@@ -1,4 +1,33 @@
 package nl.miwteam2.cryptomero.controller;
 
+import nl.miwteam2.cryptomero.domain.UserAccount;
+import nl.miwteam2.cryptomero.service.UserAccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller UserAccount
+ *
+ * @author Ercan Ersaru, studentnr: 500893336 - MIW Cohort 26
+ */
+
+@RestController
 public class UserAccountController {
+  private static final Logger logger = LoggerFactory.getLogger(UserAccountController.class);
+  private UserAccountService userAccountService;
+
+  public UserAccountController(UserAccountService service) {
+	super();
+	userAccountService = service;
+	logger.info("New UserAccountController");
+  }
+
+  @GetMapping(value = "/get_user/{id}")
+  public UserAccount getUserById(@PathVariable int id){
+	return null;
+  }
+
 }
--- src/main/java/nl/miwteam2/cryptomero/service/UserAccountService.java
+++ src/main/java/nl/miwteam2/cryptomero/service/UserAccountService.java
@@ -1,4 +1,32 @@
 package nl.miwteam2.cryptomero.service;
 
+import nl.miwteam2.cryptomero.domain.UserAccount;
+import nl.miwteam2.cryptomero.repository.JdbcUserAccountDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service UserAccount
+ *
+ * @author Ercan Ersaru, studentnr: 500893336 - MIW Cohort 26
+ */
+
+@Service
 public class UserAccountService {
+  private static final Logger logger = LoggerFactory.getLogger(UserAccountService.class);
+  private JdbcUserAccountDao jdbcUserAccountDao;
+
+//  @Autowired
+  public UserAccountService() {
+	super();
+//	jdbcUserAccountDao = dao;
+	logger.info("New UserAccountService");
+  }
+
+  public UserAccount getById(int id){
+	return jdbcUserAccountDao.findById(id);
+  }
+
 }
