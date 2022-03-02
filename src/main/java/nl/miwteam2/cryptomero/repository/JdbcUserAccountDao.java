e58b0a045fbedeea953bff78ed3e802afebf1af3
Samuel Geurts <sngeurts@gmail.com>
2022-03-02 14:08:16 +0100

    add @Repository to class and @Autowired to constructor

--- src/main/java/nl/miwteam2/cryptomero/repository/JdbcUserAccountDao.java
+++ src/main/java/nl/miwteam2/cryptomero/repository/JdbcUserAccountDao.java
@@ -3,8 +3,10 @@ package nl.miwteam2.cryptomero.repository;
 import nl.miwteam2.cryptomero.domain.UserAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,11 +18,13 @@ import java.util.List;
  * @author Ercan Ersaru, studentnr: 500893336 - MIW Cohort 26
  */
 
+@Repository
 public class JdbcUserAccountDao implements GenericDao<UserAccount> {
   private static final Logger logger = LoggerFactory.getLogger(JdbcUserAccountDao.class);
   private List<UserAccount> userList;
   JdbcTemplate jdbcTemplate;
 
+  @Autowired
   public JdbcUserAccountDao(JdbcTemplate jdbcTemplate) {
 	this.jdbcTemplate = jdbcTemplate;
 	//	userList = new ArrayList<>();
@@ -57,7 +61,7 @@ public class JdbcUserAccountDao implements GenericDao<UserAccount> {
   private class UserAccountRowMapper implements RowMapper<UserAccount> {
 	@Override
 	public UserAccount mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
-	  return new UserAccount(resultSet.getInt("idAccount"),
+	  return new UserAccount(resultSet.getInt("id_account"),
 			  resultSet.getString("email"),
 			  resultSet.getString("password"));
 	}
