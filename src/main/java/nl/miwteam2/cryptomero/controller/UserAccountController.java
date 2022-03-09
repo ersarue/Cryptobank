630b6a1f7e5dfc3675b9bb0e2f0a1e04a92f648e
ee <47424782+ee-git@users.noreply.github.com>
2022-03-09 08:15:38 +0100

    implemented GenericController, edited updateOne and storeOne and added password hashing

--- src/main/java/nl/miwteam2/cryptomero/controller/UserAccountController.java
+++ src/main/java/nl/miwteam2/cryptomero/controller/UserAccountController.java
@@ -5,6 +5,8 @@ import nl.miwteam2.cryptomero.service.UserAccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,10 +16,10 @@ import java.util.List;
  *
  * @author Ercan Ersaru, studentnr: 500893336 - MIW Cohort 26
  */
-@RequestMapping(value = "/useraccount")
 
 @RestController
-public class UserAccountController {
+@RequestMapping(value = "/useraccount")
+public class UserAccountController implements GenericController<UserAccount> {
   private static final Logger logger = LoggerFactory.getLogger(UserAccountController.class);
   private UserAccountService userAccountService;
 
@@ -28,22 +30,52 @@ public class UserAccountController {
 	logger.info("New UserAccountController");
   }
 
-  @GetMapping(value = "/{id}")
-  public UserAccount findById(@PathVariable int id){
-	return userAccountService.findById(id);
-  }
-  public UserAccount getUserById(@PathVariable int id){
-	return userAccountService.findById(id);
-  }
-
-  @GetMapping(value = "/useraccounts")
-  public List<UserAccount> getUserAccounts(){return userAccountService.getUserAccounts();}
-
-  @PutMapping(value = "/store_user")
-  public int storeUser(@RequestBody UserAccount userAccount){
-	userAccountService.storeOne(userAccount);
-	return userAccount.getIdAccount();
+  @GetMapping("/{id}")
+  public ResponseEntity<?> findById(@PathVariable int id) {
+	try {
+	  return new ResponseEntity<>(userAccountService.findById(id), HttpStatus.OK);
+	} catch (Exception exception){
+	  return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
   }
 
+  @PostMapping
+  public ResponseEntity<?> storeOne(@RequestBody UserAccount userAccount) {
+	try {
+	  return new ResponseEntity<>(userAccountService.storeOne(userAccount), HttpStatus.CREATED);
+	} catch (Exception exception) {
+	  return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+  }
+
+  @GetMapping
+  public List<UserAccount> getAll() {
+	return userAccountService.getAll();
+  }
+
+  @PutMapping
+  public ResponseEntity<?> updateOne(@RequestBody UserAccount userAccount) {
+	try {
+	  return new ResponseEntity<>(userAccountService.updateOne(userAccount), HttpStatus.OK);
+	} catch (Exception exception) {
+	  return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+  }
+
+  @DeleteMapping("/{id}")
+    public void deleteUserAccount(@PathVariable int id) {
+  	userAccountService.deleteOne(id);
+    }
+
+	// TODO ResponseEntity geeft error
+//  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteOne(@PathVariable int id) {
+	try {
+	  return null;
+//	  return new ResponseEntity<>(userAccountService.deleteOne(id), HttpStatus.OK);
+	} catch (Exception exception) {
+	  return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+  }
 
 }
