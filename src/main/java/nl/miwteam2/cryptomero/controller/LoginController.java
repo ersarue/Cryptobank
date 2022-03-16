3bb670deeacfaeb1ed87ea3de062f1163e3fd213
Mink <mink.tielenius.kruijthoff@hva.nl>
2022-03-16 16:30:05 +0100

    login_cleanup

--- src/main/java/nl/miwteam2/cryptomero/controller/LoginController.java
+++ src/main/java/nl/miwteam2/cryptomero/controller/LoginController.java
@@ -4,7 +4,6 @@ import nl.miwteam2.cryptomero.domain.Customer;
 import nl.miwteam2.cryptomero.domain.UserAccount;
 import nl.miwteam2.cryptomero.service.Authentication.AuthenticationService;
 import nl.miwteam2.cryptomero.service.Authentication.LoginService;
-import nl.miwteam2.cryptomero.service.CustomerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,46 +14,30 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  * @author MinkTK
- * @version 1.3
+ * @version 1.4
  */
 
 @CrossOrigin
 @RestController
 public class LoginController {
-
     private final LoginService loginService;
     private final AuthenticationService authenticationService;
-    private final CustomerService customerService;
 
     private final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
-    public LoginController(LoginService loginService, AuthenticationService authenticationService, CustomerService customerService) {
+    public LoginController(LoginService loginService, AuthenticationService authenticationService) {
         this.loginService = loginService;
         this.authenticationService = authenticationService;
-        this.customerService = customerService;
     }
 
-    // ROUTE with credentials in AUTHORIZATION HEADER
-    @PostMapping
-    public ResponseEntity<String> login(@RequestHeader ("Authorization") String credentials) throws Exception {
-        logger.info("new login with header attempt");
+//  MTK: ROUTE according Acceptance Criteria - return token in header
+    @PostMapping("/users/headerAuthentication")
+    public ResponseEntity<?> loginHeader(@RequestBody UserAccount userAccount) {
+        logger.info("new login attempt");
         try {
             HttpHeaders headers = new HttpHeaders();
-            headers.add("accessToken", loginService.login(credentials));
-            return new ResponseEntity<>("Login Succesful", headers, HttpStatus.OK);
-        } catch (Exception exception) {
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-//    MTK: ROUTE with credentials in REQUEST BODY
-    @PostMapping("/users/authenticate")
-    public ResponseEntity<?> login(@RequestBody UserAccount userAccount) {
-        logger.info("new login with body attempt");
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("accessToken", loginService.login(userAccount));
+            headers.add("Authorization", loginService.login(userAccount));
             Customer currentCustomer = loginService.getAuthenticatedCustomer(userAccount.getEmail());
             return new ResponseEntity<>(currentCustomer, headers, HttpStatus.OK);
         } catch (Exception exception) {
@@ -62,10 +45,21 @@ public class LoginController {
         }
     }
 
+//  MTK: ROUTE with credentials in AUTHORIZATION HEADER (basic authentication)
+    @GetMapping("/users/basicAuthentication")
+    public ResponseEntity<String> login(@RequestHeader ("Authorization") String credentials) throws Exception {
+        logger.info("new login Basic Authentication attempt");
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", loginService.login(credentials));
+            return new ResponseEntity<>("Login Succesful", headers, HttpStatus.OK);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
 
-
-    // MTK test methode for authentication purpose - to be continued
-    @GetMapping
+//  MTK: TEST methode for frontend - to be continued
+    @GetMapping("/users/loginStatus")
     public ResponseEntity<String> statusLogin(@RequestHeader ("Authorization") String jwt) {
         if (authenticationService.authenticateToken(jwt)) {
             return new ResponseEntity<>("ingelogd", HttpStatus.OK);
