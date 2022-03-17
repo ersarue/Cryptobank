8c1655535a26e3a3d13febd1ae95cb2d70da9c69
Petra Coenen <petra.coenen@hva.nl>
2022-03-17 14:56:57 +0100

    Add function to remove token from browser local storage

--- src/main/resources/static/js/tokenUtils.js
+++ src/main/resources/static/js/tokenUtils.js
@@ -2,8 +2,14 @@
 
 "use strict";
 
+// Getting the access token from the browser local storage, including 'Bearer ' prefix, to be used in Authorization header
 export const getToken = () => {
     const token = localStorage.getItem("access-token");
     // console.log("Token retrieved from localStorage:" + token);
     return `Bearer ${token}`;
+}
+
+// Removing the access token from the browser local storage
+export const removeToken = () => {
+    localStorage.removeItem("access-token");
 }
