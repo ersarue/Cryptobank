41b8bd5bf04889bb7293dc9fa7b05fca51412ad1
Samuël Geurts <samuel.geurts@hva.nl>
2022-03-23 14:40:39 +0100

    Create CustomerDto

---
+++ src/main/java/nl/miwteam2/cryptomero/domain/CustomerDto.java
@@ -0,0 +1,93 @@
+package nl.miwteam2.cryptomero.domain;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class CustomerDto {
+
+    private String firstName;
+    private String namePrefix;
+    private String lastName;
+    private LocalDate dob;
+    private String bsn;
+    private String telephone;
+    private String email;
+    private String password;
+    private Address address;
+
+
+    public CustomerDto() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getNamePrefix() {
+        return namePrefix;
+    }
+
+    public void setNamePrefix(String namePrefix) {
+        this.namePrefix = namePrefix;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getBsn() {
+        return bsn;
+    }
+
+    public void setBsn(String bsn) {
+        this.bsn = bsn;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
