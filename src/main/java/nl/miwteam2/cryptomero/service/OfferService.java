f99dd101e0a95ee33ea291381c61a7746950f473
Samuël Geurts <samuel.geurts@hva.nl>
2022-03-31 16:05:11 +0200

    set out method structure for offer matching

--- src/main/java/nl/miwteam2/cryptomero/service/OfferService.java
+++ src/main/java/nl/miwteam2/cryptomero/service/OfferService.java
@@ -1,9 +1,6 @@
 package nl.miwteam2.cryptomero.service;
 
-import nl.miwteam2.cryptomero.domain.Asset;
-import nl.miwteam2.cryptomero.domain.Customer;
-import nl.miwteam2.cryptomero.domain.Offer;
-import nl.miwteam2.cryptomero.domain.TradeOfferDto;
+import nl.miwteam2.cryptomero.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,7 +14,7 @@ import java.util.List;
 /**
  * checked offer op match en vraagt eventueel transactie aan
  * @author: Samuel Geurts en Stijn Klijn
- * Version 1.0
+ * Version 1.1
 */
 @Service
 public class OfferService {
@@ -33,14 +30,73 @@ public class OfferService {
     //methode die alles afhandelt
     public Offer tradeOffer(TradeOfferDto tradeOfferDto) {
         //todo handel offer af
+
+        List<Offer> matchList = null;
+        Transaction transaction;
+
         Offer offer = getOffer(tradeOfferDto);
-        List<Offer> offerList = getListOfOffers();
+        List<Offer> offerList = getAll();
+
+        //bij positief amount wil je verkopen
+        if (offer.getAmountOffer() > 0){
+            matchList = findBuyers(offer);
+        } else if (offer.getAmountOffer() < 0) {
+            matchList = findSellers(offer);
+        } else {
+            //todo return iets als de amount nul is
+        }
+
+        //herhaal de function totdat offer.getmount leeg is of wanneer de matchlist leeg is.
+        do {
+            transaction = ComputeTransaction(matchList,offer);
+        }
+        while ((offer.getAmountOffer() != 0) || !matchList.isEmpty());
+
+
+
+        return null;
+    }
+
+    //maakt een offer van trade Offer Dto
+    public Offer getOffer(TradeOfferDto tradeOfferDto) {
+        Offer offer = new Offer(tradeOfferDto);
+        Customer customer = customerService.findById(tradeOfferDto.getIdAccountOffer());
+        Asset asset = assetService.findByName(tradeOfferDto.getAssetNameOffer());
+        offer.setUserOffer(customer);
+        offer.setAssetOffer(asset);
+        offer.setTimestampOffer(new Timestamp(System.currentTimeMillis()));
+        return null;
+    }
+
+    public List<Offer> getAll() {
+        //todo haal alle offers op uit de database
+        return new ArrayList<>();
+    }
+
+    //deze functie moet waarschijnlijk nug in stukje opgeknipt worden.
+    public Transaction ComputeTransaction(List<Offer> matchList, Offer offer) {
+        // todo - maakt een transactie object tussen offer en het eerste aanbod in de matchlist.
+
+        // todo - probeer transactie te doen (TransactionService.....)
+        // todo - als het niet goed gaat return null
+        // todo - als het wel goed gaat update matchlist (ofwel een entry verwijdern of updaten)
+        // todo -     en update/delete OfferTable (OfferDao.update(...)) - van de opponent
+        // todo - update offer (ofwel er is nog een deel van de aanvraag over of amountOffer wordt op nul gezet.
+
+        // spreekt transactie service aan
+        // als het goed is gegaan dan update of delete offer in offerDatabase.
+
         return null;
     }
 
 
+
+    public Offer storeOne(TradeOfferDto tradeOfferDto) {
+        return null;
+    }
+
     public List<Offer> findSellers(Offer buyer) {
-        List<Offer> sellers = getAllOffers().stream().filter(e -> e.getAssetOffer().equals(buyer.getAssetOffer()) && e.getAmountOffer() > 0)
+        List<Offer> sellers = getAll().stream().filter(e -> e.getAssetOffer().equals(buyer.getAssetOffer()) && e.getAmountOffer() > 0)
                 .sorted(Comparator.comparing(Offer::getPriceOffer)).toList();
         buyer.setAmountOffer(-1 * buyer.getAmountOffer());
 
@@ -58,7 +114,7 @@ public class OfferService {
     }
 
     public List<Offer> findBuyers(Offer seller) {
-        List<Offer> buyers = getAllOffers().stream().filter(e -> e.getAssetOffer().equals(seller.getAssetOffer()) && e.getAmountOffer() < 0)
+        List<Offer> buyers = getAll().stream().filter(e -> e.getAssetOffer().equals(seller.getAssetOffer()) && e.getAmountOffer() < 0)
                 .sorted(Comparator.comparing(Offer::getPriceOffer).reversed()).toList();
         buyers.forEach(e -> e.setAmountOffer(-1 * e.getAmountOffer()));
 
@@ -73,37 +129,5 @@ public class OfferService {
             }
         }
         return matches;
-    }
-
-    public List<Offer> getAllOffers() {
-        return new ArrayList<>();
-    }
-
-    public Offer storeOne(TradeOfferDto tradeOfferDto) {
-        return null;
-    }
-
-    //maakt een offer van trade Offer Dto
-    public Offer getOffer(TradeOfferDto tradeOfferDto) {
-        Offer offer = new Offer(tradeOfferDto);
-        Customer customer = customerService.findById(tradeOfferDto.getIdAccountOffer());
-        Asset asset = assetService.findByName(tradeOfferDto.getAssetNameOffer());
-        offer.setUserOffer(customer);
-        offer.setAssetOffer(asset);
-        offer.setTimestampOffer(new Timestamp(System.currentTimeMillis()));
-        return null;
-    }
-
-    //haalt offerlijst uit database
-    public List<Offer> getListOfOffers() {
-        return null;
-    }
-
-    //haalt matches op tov offer
-    public void getMatches(List<Offer> offerList, Offer offer) {
-
-        //if niks opslaan
-
-        //if matches
     }
 }
