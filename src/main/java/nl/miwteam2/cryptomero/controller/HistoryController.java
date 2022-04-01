51b155a61c6a6a9bb3c4a55f3c40ce4885742a0c
Petra Coenen <petra.coenen@hva.nl>
2022-04-01 10:31:40 +0200

    Add user authentication to history controller

--- src/main/java/nl/miwteam2/cryptomero/controller/HistoryController.java
+++ src/main/java/nl/miwteam2/cryptomero/controller/HistoryController.java
@@ -1,11 +1,13 @@
 package nl.miwteam2.cryptomero.controller;
 
 import nl.miwteam2.cryptomero.domain.Transaction;
+import nl.miwteam2.cryptomero.service.Authentication.AuthenticationService;
 import nl.miwteam2.cryptomero.service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,15 +21,19 @@ import java.util.List;
 public class HistoryController {
 
     private HistoryService historyService;
+    private AuthenticationService authService;
 
     @Autowired
-    public HistoryController(HistoryService historyService) {
+    public HistoryController(HistoryService historyService, AuthenticationService authService) {
         this.historyService = historyService;
+        this.authService = authService;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<List<Transaction>> getAllHistory(@PathVariable int id) {
-        List<Transaction> history = historyService.getAllHistory(id);
-        return new ResponseEntity<>(history, HttpStatus.OK);
+    public ResponseEntity<List<Transaction>> getAllHistory(@PathVariable int id, @RequestHeader("Authorization") String token) {
+        if (authService.isValidToken(token)) {
+            List<Transaction> history = historyService.getAllHistory(id);
+            return new ResponseEntity<>(history, HttpStatus.OK);
+        } else throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
     }
 }
