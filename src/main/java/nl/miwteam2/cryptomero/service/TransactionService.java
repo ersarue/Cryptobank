9dd56ccb56dc882ab9f5ae78b5905d6f8d9e8b76
ee <47424782+ee-git@users.noreply.github.com>
2022-04-01 21:45:01 +0200

    edit methodes to handle tradeWithBank methode

--- src/main/java/nl/miwteam2/cryptomero/service/TransactionService.java
+++ src/main/java/nl/miwteam2/cryptomero/service/TransactionService.java
@@ -5,6 +5,8 @@ import nl.miwteam2.cryptomero.repository.TransactionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 /**
  * TransactionService
@@ -14,56 +16,95 @@ import org.springframework.stereotype.Service;
 @Service
 public class TransactionService {
   private static final Logger LOGGER = LoggerFactory.getLogger(TransactionService.class);
-  private final double TRANSACTION_FEE = 1.5;
   private final TransactionRepository transactionRepository;
   private final BankAccountService bankAccountService;
+  private final AssetService assetService;
+  private final Customer bank;
+  private final RateService rate;
 
-  public TransactionService(TransactionRepository transactionRepository, BankAccountService bankAccountService) {
+  public TransactionService(TransactionRepository transactionRepository,
+							BankAccountService bankAccountService, AssetService assetService,
+							CustomerService customerService, RateService rate) {
 	super();
 	LOGGER.info("New TransactionService");
 	this.transactionRepository = transactionRepository;
 	this.bankAccountService = bankAccountService;
+	this.assetService = assetService;
+	this.bank = customerService.findById(1);
+	this.rate = rate;
   }
-  
+
   public Transaction tradeWithBank(TradeBankDto trade) throws Exception {
-	// buy from bank
-	if (trade.getAmountTrade() > 0) {
+	if (trade.getAmountTrade() > 0) { // buy from bank
 	  if (checkBalance(trade.getCustomer().getBankAccount().getBalanceEur(), trade.getAmountTrade())) {
 		throw new Exception("Cannot buy from bank. Insufficient balance");
 	  }
-	  updateBankAccount(trade.getCustomer(), trade.getAmountTrade());
-	  // TODO update wallet
+	  // call completeTradeBankDto
+	  completeTradeBankDto(trade);
+	  // update wallet
+	  updateWallet(trade);
+	  // update bank account
+	  updateBankAccount(trade);
 	  // TODO call storeTransaction
-	} else {
-	  // sell to bank
-		if (checkWallet(String.valueOf(trade.getCustomer().getWallet()), trade.getAmountTrade())) {
-		  throw new Exception("Cannot sell to bank. Insufficient balance");
-		}
-	  // TODO update bank wallet
+	} else {  // sell to bank
+	  // check asset in wallet
+	  if (!(trade.getCustomer().getWallet().containsKey(trade.getAssetNameTrade()))) {
+		throw new Exception("Cannot sell to bank. Insufficient balance");
+	  }
+	  // call completeTradeBankDto
+	  completeTradeBankDto(trade);
+	  // update bank wallet
+	  updateBankAccount(trade);
 	  // TODO call storeTransaction
 	}
-    return null;
+	// TODO finish return
+	return null;
   }
 
   public void storeTransaction(Transaction transaction) {
 	transactionRepository.storeOne(transaction);
   }
 
-
-  public void updateBankAccount(Customer customer, double amount) {
-	customer.getBankAccount().setBalanceEur(customer.getBankAccount().getBalanceEur() - amount);
-	bankAccountService.updateOne(customer.getBankAccount());
+  public boolean IsBuyFromBank(double amount) {
+	return amount > 0;
   }
 
-  public void updateWallet(Customer customer, double amount) {
+  // completes the trade with calculated Euro amount and transaction fee
+  public void completeTradeBankDto(TradeBankDto tradeBankDto) {
+	final double TRANSACTION_FEE = 1.5;
+	new Transaction(LocalDateTime.now(), bank, tradeBankDto.getCustomer(),
+			assetService.findByName(tradeBankDto.getAssetNameTrade()), tradeBankDto.getAmountTrade(),
+			calculateEuroByAmountTrade(tradeBankDto.getAmountTrade(),
+					rate.getLatestByName(tradeBankDto.getAssetNameTrade()).getRate()), TRANSACTION_FEE);
+  }
+
+  // calculates Euro amount with user's input for requested amount
+  public double calculateEuroByAmountTrade(double amount, double rate) {
+	return amount * rate;
+  }
+
+
+  // update bank balance with user input
+  public void updateBankAccount(TradeBankDto trade) {
+	BankAccount bankAccount = trade.getCustomer().getBankAccount();
+	bankAccount.setBalanceEur(bankAccount.getBalanceEur() - trade.getAmountTrade());
+	bankAccountService.updateOne(bankAccount);
+  }
+
+  // loop over Wallet, if asset in wallet then update, if not then add
+  public void updateWallet(TradeBankDto trade) {
+	Map<String, Double> wallet = trade.getCustomer().getWallet();
+	if (wallet.containsKey(trade.getAssetNameTrade())) {
+	  wallet.put(trade.getAssetNameTrade(), wallet.get(trade.getAssetNameTrade()) + trade.getAmountTrade());
+	} else {
+	  wallet.put(trade.getAssetNameTrade(), trade.getAmountTrade());
+	}
   }
 
   public boolean checkBalance(double balance, double amount) {
 	return !(balance > amount);
   }
 
-  public boolean checkWallet(String type, double amount) {
-	return true;
-  }
 }
+
 
