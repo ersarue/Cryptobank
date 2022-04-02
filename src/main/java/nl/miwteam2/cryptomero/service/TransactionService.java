026215686c6a2f6557a472fce9a0cd5f6bae263c
Samuël Geurts <samuel.geurts@hva.nl>
2022-04-02 07:38:29 +0200

    Add method tradeWithUser to TransactionService

--- src/main/java/nl/miwteam2/cryptomero/service/TransactionService.java
+++ src/main/java/nl/miwteam2/cryptomero/service/TransactionService.java
@@ -2,9 +2,12 @@ package nl.miwteam2.cryptomero.service;
 
 import nl.miwteam2.cryptomero.domain.*;
 import nl.miwteam2.cryptomero.repository.TransactionRepository;
+import nl.miwteam2.cryptomero.repository.WalletDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * TransactionService
@@ -17,12 +20,14 @@ public class TransactionService {
   private final double TRANSACTION_FEE = 1.5;
   private final TransactionRepository transactionRepository;
   private final BankAccountService bankAccountService;
+  private WalletDao walletDao;
 
-  public TransactionService(TransactionRepository transactionRepository, BankAccountService bankAccountService) {
+  public TransactionService(TransactionRepository transactionRepository, BankAccountService bankAccountService, WalletDao walletDao) {
 	super();
 	LOGGER.info("New TransactionService");
 	this.transactionRepository = transactionRepository;
 	this.bankAccountService = bankAccountService;
+    this.walletDao = walletDao;
   }
   
   public Transaction tradeWithBank(TradeBankDto trade) throws Exception {
@@ -43,6 +48,63 @@ public class TransactionService {
 	  // TODO call storeTransaction
 	}
     return null;
+  }
+
+  public Transaction tradeWithUser(TransactionDto transactionDto) throws Exception {
+      //todo cut method in pieces
+
+      // make transaction from transactionDto
+      Transaction transaction = new Transaction(transactionDto);
+      // compute fee and add to transaction object
+      double fee = transaction.totalPrice() * 0.0165; //todo use final
+      transaction.setEurFee(fee);
+
+      String assetName = transaction.getAsset().getAssetName();
+      Map<String,Double> walletAssetGiver = transaction.getAssetGiver().getWallet();
+      Map<String,Double> walletAssetRecipient = transaction.getAssetRecipient().getWallet();
+      BankAccount bankAccountAssetGiver = transaction.getAssetGiver().getBankAccount();
+      //userAccount is null because of JsonBackReference
+      bankAccountAssetGiver.setUserAccount(transaction.getAssetGiver());
+      BankAccount bankAccountAssetRecipient = transaction.getAssetRecipient().getBankAccount();
+      //userAccount is null because of JsonBackReference
+      bankAccountAssetRecipient.setUserAccount(transaction.getAssetRecipient());
+
+      // check balance of both customers
+      System.out.println(4);
+      System.out.println("asset_amount: " + transaction.getAssetRecipient().getWallet().get(assetName));
+
+      double assetAmountRecipient = walletAssetRecipient.get(assetName); //asset_amount assetRecipient
+      double assetAmountGiver = walletAssetGiver.get(assetName); //asset_amount assetGiver
+      double BalanceEurGiver = bankAccountAssetGiver.getBalanceEur(); //BalanceEur assetGiver
+      double BalanceEurRecipient = bankAccountAssetRecipient.getBalanceEur(); //BalanceEur assetGiver
+
+      if (checkBalance(assetAmountGiver, transaction.getAssetAmount())) {
+          throw new Exception("Cannot trade Asset. Insufficient Asset balance");
+      } else if (checkBalance(BalanceEurGiver, transaction.getEurFee() / 2)) {
+          throw new Exception("Cannot trade Asset. Insufficient balance Giver to pay fee");
+      } else if (checkBalance(BalanceEurRecipient, transaction.totalPrice() + transaction.getEurFee() / 2)) {
+          throw new Exception("Cannot trade Asset. Insufficient balance Recipient");
+      }
+      System.out.println(5);
+
+      // change wallet balance Giver and store
+      walletAssetGiver.put(assetName,walletAssetGiver.get(assetName) - transaction.getAssetAmount());
+      walletDao.updateOne(walletAssetGiver);
+      // change wallet balance Recipient
+      walletAssetRecipient.put(assetName,walletAssetRecipient.get(assetName) + transaction.getAssetAmount());
+      walletDao.updateOne(walletAssetRecipient);
+
+      System.out.println(6);
+      // change saldo Giver
+      bankAccountAssetGiver.setBalanceEur(bankAccountAssetGiver.getBalanceEur() + transaction.totalPrice() - fee/2);
+      bankAccountService.updateOne(bankAccountAssetGiver);
+      // change saldo Recipient
+      bankAccountAssetRecipient.setBalanceEur(bankAccountAssetRecipient.getBalanceEur() - (transaction.totalPrice() + fee/2));
+      bankAccountService.updateOne(bankAccountAssetRecipient);
+      // store transaction
+      int transactionId = transactionRepository.storeOne(transaction);
+
+      return transaction;
   }
 
   public void storeTransaction(Transaction transaction) {
