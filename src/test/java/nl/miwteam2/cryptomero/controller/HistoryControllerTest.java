6d7f2b2bce794274e46fec70246008c2aa3374fb
Petra Coenen <petra.coenen@hva.nl>
2022-04-02 14:33:33 +0200

    Add first setup for HistoryController test

---
+++ src/test/java/nl/miwteam2/cryptomero/controller/HistoryControllerTest.java
@@ -0,0 +1,45 @@
+package nl.miwteam2.cryptomero.controller;
+
+import nl.miwteam2.cryptomero.service.Authentication.AuthenticationService;
+import nl.miwteam2.cryptomero.service.HistoryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.*;
+
+@WebMvcTest(HistoryController.class)
+class HistoryControllerTest {
+
+    private MockMvc mockServer;
+
+    @MockBean
+    private HistoryService mockHistoryService;
+    @MockBean
+    private AuthenticationService mockAuthService;
+
+    @Autowired
+    public HistoryControllerTest(MockMvc mockServer) {
+        super();
+        this.mockServer = mockServer;
+    }
+
+    @Test
+    void testGetAllHistory() {
+        MockHttpServletRequestBuilder request;
+        request = MockMvcRequestBuilders.get("/testHistory/1");
+        try {
+            MockHttpServletResponse response = mockServer.perform(request).andReturn().getResponse();
+            assertThat((response).getStatus()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.getContentAsString()).startsWith("[]");
+        } catch (Exception e) {
+            System.out.println("Test failed. \n" + e.getMessage());
+        }
+    }
+}
