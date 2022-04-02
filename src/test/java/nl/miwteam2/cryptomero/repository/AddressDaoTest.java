bad6e33887e04671c8c1db2c0178ff98e625c251
Petra Coenen <petra.coenen@hva.nl>
2022-04-02 13:23:48 +0200

    Adapt AddressDao tests to updated H2 db data

--- src/test/java/nl/miwteam2/cryptomero/repository/AddressDaoTest.java
+++ src/test/java/nl/miwteam2/cryptomero/repository/AddressDaoTest.java
@@ -14,7 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
 * @author Petra Coenen
-* @version 1.1
 */
 
 @SpringBootTest
@@ -29,6 +28,7 @@ public class AddressDaoTest {
 
     @Autowired
     public AddressDaoTest(GenericDao<Address> AddressDao) {
+        super();
         this.daoUnderTest = AddressDao;
     }
 
@@ -40,11 +40,11 @@ public class AddressDaoTest {
 
     @Test
     void storeOne() {
-        int expected = 3;
+        int expected = 4;
         int actual = daoUnderTest.storeOne(addressAdam);
         assertThat(actual).isNotNull().isEqualTo(expected);
 
-        expected = 4;
+        expected = 5;
         actual = daoUnderTest.storeOne(addressHague);
         assertThat(actual).isNotNull().isEqualTo(expected);
     }
@@ -61,7 +61,7 @@ public class AddressDaoTest {
     void getAll() {
         List<Address> allAdresses = daoUnderTest.getAll();
         assertThat(allAdresses).isNotNull().isNotEmpty();
-        assertThat(allAdresses.size()).isEqualTo(2);
+        assertThat(allAdresses.size()).isEqualTo(3);
     }
 
     @Test
@@ -80,7 +80,7 @@ public class AddressDaoTest {
     @Test
     void deleteOne() {
         int expectedReturn = 1;
-        int actualReturn = daoUnderTest.deleteOne(2);
+        int actualReturn = daoUnderTest.deleteOne(3);
         assertThat(actualReturn).isNotNull().isEqualTo(expectedReturn);
     }
 }
