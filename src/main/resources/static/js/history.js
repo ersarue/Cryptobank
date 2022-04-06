0b15ce0ba205e64a1243aa0c35c5d806fcd6cf13
ee <47424782+ee-git@users.noreply.github.com>
2022-04-06 21:55:41 +0200

    edit fetch methode and styling for history

--- src/main/resources/static/js/history.js
+++ src/main/resources/static/js/history.js
@@ -3,7 +3,7 @@
 'use strict'
 
 import {includeHTML, addLogout} from "./includeHTML.js";
-import {getToken, removeToken} from "./tokenUtils.js";
+import {getToken} from "./tokenUtils.js";
 
 // logout function
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,101 +11,88 @@ document.addEventListener('DOMContentLoaded', () => {
     addLogout()
 })
 
-const url = 'http://localhost:8080/history';
-
-// fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//         // code to handle the response
-//         data.map((transaction)=>{
-//             let tr = createNode('tr'),
-//                 th = createNode('th'),
-//                 td = createNode('td');
-//             th.innerText = transaction.transactienummer;
-//             td.innerText = transaction.transactiedatum;
-//             td.innerText = transaction.gever;
-//             td.innerText = transaction.ontvanger;
-//             td.innerText = transaction.assetnaam;
-//             td.innerText = transaction.hoeveelheid;
-//             td.innerText = transaction.bedrag;
-//             td.innerText = transaction.transactiekost;
-//             appendNode(tr, th);
-//             appendNode(tr, td);
-//             appendNode(tr, td);
-//             appendNode(tr, td);
-//             appendNode(tr, td);
-//             appendNode(tr, td);
-//             appendNode(tr, td);
-//             appendNode(tr, td);
-//         })
-//     }).catch(err => {
-//     console.error('Error: ', err);
-// });
-
 // create an element
 const createNode = (elem) => {
     return document.createElement(elem);
 };
 
 // append an element to parent
-const appendNode = (parent, elem) => {
-    parent.appendChild(elem);
+const appendNode = (parent, child) => {
+    parent.appendChild(child);
 }
 
+// fill content of element
+const fillContent = (elem, content) => {
+    elem.innerText = content;
+}
+
+// change date time format to Dutch local format
+const getDutchDateFormat = (dateString) => {
+    const date = new Date(dateString);
+    const options = { weekday:'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    return date.toLocaleDateString('nl-NL', options);
+}
+
+// retrieve transactions data from database
 function retrieveTransactions(transactions) {
     for (let transaction in transactions) {
-        // console.log(transactions[transaction].idTransaction);
         const transactienummer = transactions[transaction].idTransaction;
         const transactiedatum = transactions[transaction].transactionTime;
-        const gever = transactions[transaction].assetGiver.firstName + " " + transactions[transaction].assetGiver.lastName;
-        const ontvanger = transactions[transaction].assetRecipient.firstName + " " + transactions[transaction].assetRecipient.lastName;
+        const gever = transactions[transaction].assetGiver;
+        const geverVolledigeNaam = gever.firstName + " " + gever.lastName;
+        const ontvanger = transactions[transaction].assetRecipient
+        const ontvangerVolledigeNaam = ontvanger.firstName + " " + ontvanger.lastName;
         const assetnaam = transactions[transaction].asset.assetName;
         const hoeveelheid = transactions[transaction].assetAmount;
         const bedrag = transactions[transaction].eurAmount;
         const transactiekost = transactions[transaction].eurFee;
 
-        const rowElement = document.createElement("tr");
-        const cellElement1 = document.createElement("th");
-        cellElement1.innerHTML = transactienummer;
-        const cellElement2 = document.createElement("td");
-        cellElement2.innerHTML = transactiedatum;
-        const cellElement3 = document.createElement("td");
-        cellElement3.innerHTML = gever;
-        const cellElement4 = document.createElement("td");
-        cellElement4.innerHTML = ontvanger;
-        const cellElement5 = document.createElement("td");
-        cellElement5.innerHTML = assetnaam;
-        const cellElement6 = document.createElement("td");
-        cellElement6.innerHTML = hoeveelheid;
-        const cellElement7 = document.createElement("td");
-        cellElement7.innerHTML = bedrag;
-        const cellElement8 = document.createElement("td");
-        cellElement8.innerHTML = transactiekost;
+        const rowElement = createNode("tr");
+        rowElement.classList.add('bodyRow');
+        const cellElement1 = createNode("td");
+        fillContent(cellElement1, transactienummer);
+        const cellElement2 = createNode("td");
+        fillContent(cellElement2, getDutchDateFormat(transactiedatum));
+        const cellElement3 = createNode("td");
+        fillContent(cellElement3, geverVolledigeNaam);
+        rowElement.classList.add('gever');
+        const cellElement4 = createNode("td");
+        fillContent(cellElement4, ontvangerVolledigeNaam);
+        rowElement.classList.add('ontvanger');
+        const cellElement5 = createNode("td");
+        fillContent(cellElement5, assetnaam);
+        const cellElement6 = createNode("td");
+        fillContent(cellElement6, hoeveelheid);
+        const cellElement7 = createNode("td");
+        fillContent(cellElement7, bedrag);
+        const cellElement8 = createNode("td");
+        fillContent(cellElement8, transactiekost);
 
-        rowElement.appendChild(cellElement1)
-        rowElement.appendChild(cellElement2)
-        rowElement.appendChild(cellElement3)
-        rowElement.appendChild(cellElement4)
-        rowElement.appendChild(cellElement5)
-        rowElement.appendChild(cellElement6)
-        rowElement.appendChild(cellElement7)
-        rowElement.appendChild(cellElement8)
+        appendNode(rowElement, cellElement1)
+        appendNode(rowElement, cellElement2)
+        appendNode(rowElement, cellElement3)
+        appendNode(rowElement, cellElement4)
+        appendNode(rowElement, cellElement5)
+        appendNode(rowElement, cellElement6)
+        appendNode(rowElement, cellElement7)
+        appendNode(rowElement, cellElement8)
 
         const transactionsTable = document.querySelector("#transactionContent")
-        transactionsTable.appendChild(rowElement);
+        appendNode(transactionsTable, rowElement);
 
     }
 }
 
+const url = new URL(window.location.href)
+
 Promise.resolve(
-    await fetch(url, {
+    await fetch(`${url.origin}/history`, {
         method: 'GET',
         headers: {'Content-Type': 'application/json', 'Authorization': getToken()}
     })
 )
     .then(r => r.json())
     .then(transactions => {
-        // console.log(transactions);
         retrieveTransactions(transactions)
     })
-    .catch(error => console.error("Transactions cannot be loaded!", error))
+    .catch(error => console.error("Something went wrong. Transactions cannot be loaded!", error));
