3d9ce12dc785a40a5ea152ece0bbe825c18ee363
Marcel Brachten <marcel.brachten@hva.nl>
2022-04-06 15:44:11 +0200

    banktransactie en offer opslaan vanuit modal

--- src/main/resources/static/js/doTrade.js
+++ src/main/resources/static/js/doTrade.js
@@ -18,6 +18,99 @@ document.addEventListener('DOMContentLoaded', () => {
 document.querySelector('#inputGroupSelect01').addEventListener('click', ()=>{
     zoekKoers(document.querySelector("#inputGroupSelect01").value)
 })
+//bepaalt eerst BANK of Marktplaats , alleen bank werkt 
+document.querySelector('#actieButton').addEventListener('click', ()=>{
+    const radiobuttonBank = document.querySelector('#btnradio1')
+    if (radiobuttonBank.checked){
+        storeBankTransactie()
+    }else{
+        storeOffer()
+    }
+        
+})
+//er wordt gecheckt of er een koopooffer of verkoopoffer geplaatst wil worden
+function samenstellenOffer(){
+    const radiobuttonKoop = document.querySelector('#btnradio3')
+    const assetName =  document.querySelector('#inputGroupSelect01').value
+    let prijsOffer = document.querySelector('#offerPrijs').value
+    let amount =  document.querySelector('#amountAsset').value * 1
+    if (radiobuttonKoop.checked){
+        amount = amount * -1
+    }
+    let data = {
+        "assetNameOffer": assetName,
+        "amountOffer": amount,
+        "rateOffer": prijsOffer
+        };
+    console.log(data)
+    return data
+}
+//de offer op marktplaats wordt gestored, ER zijn geen checks
+function storeOffer(){
+    let dataOffer = samenstellenOffer()
+    fetch('http://localhost:8080/trade/offer', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJBY2NvdW50IjoyLCJpc3MiOiJDcnlwdG9tZXJvIiwiZXhwIjoxNjQ5MjU2MjY3fQ.dSa7JY646zNv-SRZd7xIrA3NptYBd3X54SCKQoYtq2g',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataOffer)  // moet worden omgezet naar een string
+        })
+            .then(response => {
+                    if (response.status===400){
+                        alert("foutmelding")
+                    }
+                    return response.json()
+                }
+            )
+            .then(data => {
+                console.log(data)
+            });
+    }
+
+
+
+
+//er wordt gecheckt of er een koop of verkoop met de bank plaatsvindt
+function samenstellenDataBankTransactie(){
+    const radiobuttonKoop = document.querySelector('#btnradio3')
+    const assetName =  document.querySelector('#inputGroupSelect01').value
+    let amount =  document.querySelector('#amountAsset').value
+    if (radiobuttonKoop.checked){
+        amount = amount * -1
+    }
+    let data = {
+        "assetNameTrade": assetName,
+        "amountTrade": amount
+        };
+    return data
+}
+//de transactie met de bank wordt gestored, ER zijn geen checks
+function storeBankTransactie(){
+    let data = samenstellenDataBankTransactie()
+    fetch('http://localhost:8080/trade/bank', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJBY2NvdW50IjoyLCJpc3MiOiJDcnlwdG9tZXJvIiwiZXhwIjoxNjQ5MjU2MjY3fQ.dSa7JY646zNv-SRZd7xIrA3NptYBd3X54SCKQoYtq2g',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)  // moet worden omgezet naar een string
+        })
+            .then(response => {
+                    if (response.status===400){
+                        alert("foutmelding")
+                    }
+                    return response.json()
+                }
+            )
+            .then(data => {
+                console.log(data)
+            });
+    }
+
+
 //wordt opgeroepen als klant klikt op dropdownmenu
 function zoekKoers(naamCrypto) {
     fetch('http://localhost:8080/rates/latest/'+naamCrypto, {
