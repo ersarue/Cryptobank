0dad5ee4877601cc1251e79e41ae2cbffc146143
Petra Coenen <petra.coenen@hva.nl>
2022-04-09 10:45:41 +0200

    Tidy up loggers and javadoc comments

--- src/main/java/nl/miwteam2/cryptomero/repository/TransactionDao.java
+++ src/main/java/nl/miwteam2/cryptomero/repository/TransactionDao.java
@@ -1,6 +1,8 @@
 package nl.miwteam2.cryptomero.repository;
 
 import nl.miwteam2.cryptomero.domain.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -17,14 +19,17 @@ import java.util.List;
 @Repository
 public class TransactionDao {
 
+    private final Logger logger = LoggerFactory.getLogger(TransactionDao.class);
+
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
     public TransactionDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        logger.info("New TransactionDao");
     }
 
-    // Creates a customer with a primitive id for asset giver, asset recipient and asset
+    // Creates a customer with an int id for the fields "asset giver", "asset recipient" and "asset"
     private RowMapper<Transaction> transactionRowMapper = (r, rowNum) -> new Transaction(
             r.getInt("id_transaction"),
             LocalDateTime.parse(r.getString("datetime"),
@@ -34,7 +39,8 @@ public class TransactionDao {
             r.getDouble("eur_fee"));
 
     public int storeOne(Transaction t) {
-        String sql = "INSERT INTO transaction(datetime, asset_giver, asset_recipient, asset_name, asset_amount, eur_amount, eur_fee) VALUES(?, ?, ?, ?, ?, ?, ?);";
+        String sql = "INSERT INTO transaction(datetime, asset_giver, asset_recipient, asset_name, asset_amount, " +
+                "eur_amount, eur_fee) VALUES(?, ?, ?, ?, ?, ?, ?);";
         return jdbcTemplate.update(sql, t.getTransactionTime(), t.getAssetGiver().getIdAccount(),
                 t.getAssetRecipient().getIdAccount(), t.getAsset().getAssetName(), t.getAssetAmount(),
                 t.getEurAmount(), t.getEurFee());
