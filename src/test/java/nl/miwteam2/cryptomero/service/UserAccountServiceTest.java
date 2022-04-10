59354d3b8325c4d1d935b70c93a4d9cf86dfabb2
ee <47424782+ee-git@users.noreply.github.com>
2022-04-10 22:26:54 +0200

    add UserAccountServiceTest

--- src/test/java/nl/miwteam2/cryptomero/service/UserAccountServiceTest.java
+++ src/test/java/nl/miwteam2/cryptomero/service/UserAccountServiceTest.java
@@ -1,54 +1,72 @@
-//package nl.miwteam2.cryptomero.service;
-//
-//import nl.miwteam2.cryptomero.domain.UserAccount;
-//import nl.miwteam2.cryptomero.repository.GenericDao;
-//import nl.miwteam2.cryptomero.repository.JdbcUserAccountDao;
-//import nl.miwteam2.cryptomero.repository.RootRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-///**
-// * Test UserAccountService
-// *
-// * @author Ercan Ersaru, studentnr: 500893336 - MIW Cohort 26
-// */
-//
-//class UserAccountServiceTest {
-//
-//  private JdbcUserAccountDao jdbcUserAccountDao = Mockito.mock(JdbcUserAccountDao.class);
-//  private RootRepository rootRepository = Mockito.mock(RootRepository.class);
-//  private GenericDao<UserAccount> userAccountDaoMock = Mockito.mock(JdbcUserAccountDao.class);
-//  private UserAccountService userAccountServiceTest;
-//
-//  UserAccountServiceTest() {
-//	userAccountServiceTest = new UserAccountService(rootRepository, userAccountDaoMock);
-//  }
-//
-//  @BeforeEach
-//  void setUp() {
-//	UserAccount userAccount = new UserAccount(1, "test1@test.com", "test1");
-////	UserAccount userAccount2 = new UserAccount(2, "test2@test.com", "test2");
-//	Mockito.when(rootRepository.findUserAccountById(1)).thenReturn(userAccount);
-////	Mockito.when(rootRepository.findUserAccountById(2)).thenReturn(userAccount);
-//  }
-//
-//  @Test
-//  void findById() {
-//	UserAccount actualUserAccount1 = userAccountServiceTest.findById(1);
-////	UserAccount actualUserAccount2 = userAccountServiceTest.findById(2);
-//	UserAccount expectedUserAccount1 = new UserAccount(1, "test1@test.com", "test1");
-////	UserAccount expectedUserAccount2 = new UserAccount(2, "test2@test.com", "test2");
-//	expectedUserAccount1.setIdAccount(1);
-////	expectedUserAccount1.setIdAccount(2);
-//	assertThat(actualUserAccount1).isNotNull().isEqualTo(expectedUserAccount1);
-////	assertThat(actualUserAccount2).isNotNull().isEqualTo(expectedUserAccount2);
-//
-//  }
-//
-//  @Test
-//  void storeOne() {
-//  }
-//}
+package nl.miwteam2.cryptomero.service;
+
+import nl.miwteam2.cryptomero.domain.UserAccount;
+import nl.miwteam2.cryptomero.repository.RootRepository;
+import nl.miwteam2.cryptomero.repository.UserAccountDao;
+import nl.miwteam2.cryptomero.service.Authentication.HashService;
+import nl.miwteam2.cryptomero.service.Authentication.SaltMaker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserAccountServiceTest {
+
+  private UserAccountDao userAccountDao = Mockito.mock(UserAccountDao.class);
+  private RootRepository rootRepository = Mockito.mock(RootRepository.class);
+  private UserAccountService userAccountServiceToTest;
+  private UserAccount userAccountToStore;
+  private HashService hash;
+  private SaltMaker salt;
+
+  private UserAccountServiceTest(){
+    this.userAccountServiceToTest = new UserAccountService(hash, salt, rootRepository, userAccountDao);
+  }
+
+  @BeforeEach
+  void setUp() {
+    UserAccount userAccountWithId = new UserAccount(1, "test@test.com", "test");
+    UserAccount userAccountWithEmail = new UserAccount("test2@test.com", "test2");
+    userAccountToStore = new UserAccount("test123", "test123");
+    userAccountToStore.setIdAccount(123);
+    Mockito.when(rootRepository.findUserAccountById(1)).thenReturn(userAccountWithId);
+    Mockito.when(rootRepository.findUserAccountByEmail("test@test.com")).thenReturn(userAccountWithEmail);
+    Mockito.when(rootRepository.findUserAccountById(123)).thenReturn(userAccountToStore);
+  }
+
+  @Test
+  void findById() {
+    UserAccount actual = rootRepository.findUserAccountById(1);
+    UserAccount expected = new UserAccount(1, "test@test.com", "test");
+    assertThat(actual).isNotNull().isEqualTo(expected);
+  }
+
+  @Test
+  void findByEmail() {
+    UserAccount actual = rootRepository.findUserAccountByEmail("test@test.com");
+    UserAccount expected = new UserAccount("test2@test.com", "test2");
+    assertThat(actual).isNotNull().isEqualTo(expected);
+  }
+
+  @Test
+  void storeOne() {
+    UserAccount actual = userAccountServiceToTest.storeOne(userAccountToStore);
+    assertThat(actual).isNotNull();
+    assertThat(actual.getIdAccount()).isEqualTo(123);
+    assertThat(actual.getEmail()).isEqualTo("test123@test.com");
+    assertThat(actual.getPassword()).isEqualTo("test123");
+  }
+
+  @Test
+  void getAll() {
+    List<UserAccount> actual = userAccountServiceToTest.getAll();
+    List<UserAccount> expected = new ArrayList<>();
+    assertThat(actual).isNotNull().isEqualTo(expected);
+  }
+}
